f894ee3a89cf200c425a03f77b2383d4a4667407
Shurubov <shurubov@gmail.com>
2015-12-05 18:34:16 +0300

    Initial commit

---
+++ src/Hello_World.java
@@ -0,0 +1,11 @@
+/**
+ * My_First_Project created by Admin on 05.12.2015 18:31.
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        String a = "Hello World";
+        System.out.println(a);
+    }
+}
